357133e3718e103fcadfe2d4bdc9588b75f877d8
CarvalhoDevMAX <gustavo.gcj09@gmail.com>
2026-09-05 19:50:26 -0300

    fix: permitir CORS no Live Preview

--- LifeFit-backend/servidor.js
+++ LifeFit-backend/servidor.js
@@ -12,7 +12,9 @@ const FRONTEND_ORIGINS = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
   "http://127.0.0.1:5500",
-  "http://localhost:5500"
+  "http://localhost:5500",
+   "http://127.0.0.1:3002",
+  "http://localhost:3002"
 ];
 
 app.use((req, res, next) => {
